9cd5c5ae0219efc29a6b51ac156f21f5a68c7347
Dotennin <wenhua4438@gmail.com>
2024-01-20 21:45:26 +0900

    feat: one key to scale video

--- frame-export.sh
+++ frame-export.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 FILE_NAME=$1
 FPS=$2
+SUFFIX=webp
 
 ./validation.sh $FILE_NAME
 
@@ -11,8 +12,7 @@ fi
 
 PROGRESSIVE=$(ffmpeg -i $FILE_NAME 2>&1 | sed -n 's/.*(\([^,]*\), \([a-zA-Z0-9]*\)(\(.*\))/\2/p' | cut -d ',' -f1)
 DIR_NAME=$FILE_NAME-$PROGRESSIVE-$FPS-frames
-mkdir $DIR_NAME
-
+mkdir -p $DIR_NAME
 
 echo "==================="
 echo "Exporting frames for $FILE_NAME..."
@@ -20,3 +20,15 @@ echo "FPS: $FPS"
 echo "progressive: $PROGRESSIVE"
 echo "==================="
 time ffmpeg -i "$FILE_NAME" -vf fps=$FPS $DIR_NAME/frame-%03d.png
+
+mkdir -p scalled-$DIR_NAME
+time ./realesrgan-ncnn-vulkan \
+        -i $DIR_NAME \
+        -o scalled-$DIR_NAME \
+        -n realesrgan-x4plus \
+        -f $SUFFIX \
+        -v \
+	#-j 2:2:2 \
+	#-j 4:4:4 \
+
+./image-to-video.sh $FILE_NAME $FPS $SUFFIX
--- image-to-video.sh
+++ image-to-video.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 FILE_NAME=$1
 FPS=$2
-IMAGE_EXTENSION=jpg
+IMAGE_EXTENSION=$3
 
 ./validation.sh $FILE_NAME
 
@@ -10,8 +10,13 @@ then
   FPS=$(ffmpeg -i "$FILE_NAME" 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")
 fi
 
+if [ -z "$IMAGE_EXTENSION" ]
+then
+  IMAGE_EXTENSION=jpg
+fi
+
 PROGRESSIVE=$(ffmpeg -i $FILE_NAME 2>&1 | sed -n 's/.*(\([^,]*\), \([a-zA-Z0-9]*\)(\(.*\))/\2/p' | cut -d ',' -f1)
-DIR_NAME=$FILE_NAME-$PROGRESSIVE-$FPS-frames/upscayl_RealESRGAN_General_x4_v3_x4
+DIR_NAME=scalled-$FILE_NAME-$PROGRESSIVE-$FPS-frames
 
 
 echo "==================="
@@ -31,7 +36,7 @@ echo "Extacting origin video audio from $FILE_NAME..."
 time ffmpeg -i $FILE_NAME -q:a 0 -map a ./_output-audio.aac
 
 echo "Merging video and audio..."
-time ffmpeg -i ./_output.mp4 -i ./_output-audio.aac -c copy -map 0:v:0 -map 1:a:0 ./scalled-$FILE_NAME
+ffmpeg -i ./_output.mp4 -i ./_output-audio.aac -c copy -map 0:v:0 -map 1:a:0 ./scalled-$FILE_NAME
 
 echo "Done!"
 echo "Do you need to delete temporary files? (y/n)"
